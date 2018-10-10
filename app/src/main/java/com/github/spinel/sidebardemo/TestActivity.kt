a8b1271586c3e724b8cb56a1326e07c591ec30b7
Spinel <2459075@qq.com>
2018-10-10 15:51:29 +0800

    init

--- app/src/main/java/com/github/spinel/sidebardemo/TestActivity.kt
+++ app/src/main/java/com/github/spinel/sidebardemo/TestActivity.kt
@@ -9,6 +9,7 @@ class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_test)
 
         sideBar.initials = (arrayOf("A", "B", "C", "D", "E", "F", "春", "夏", "秋", "冬", "E", "F", "#"))
